1a1c407528dedd1a3e4b0894db4d1189ed11f767
dipali914 <dipaliwohlig@gmail.com>
2016-10-17 10:33:31 +0530

    serverCopy

--- config/env/production.js
+++ config/env/production.js
@@ -25,7 +25,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  // port: 91,
+  port: 91,
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
