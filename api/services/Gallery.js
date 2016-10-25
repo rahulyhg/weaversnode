e2b0360e2163a1f66ad661a449101845ea41b664
Pooja <pthakare33@gmail.com>
2016-10-25 14:20:28 +0530

    git ignore

--- api/services/Gallery.js
+++ api/services/Gallery.js
@@ -10,28 +10,14 @@ require('mongoose-middleware').initialize(mongoose);
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-  name: {
-      type: String,
-      default: ""
-  },
-  status: {
-      type: String,
-      enum: ["true", "false"]
-  },
-  images: [{
-      image: {
-          type: String,
-          default: ""
-      },
-      order: {
-          type: Number,
-          default: 0
-      },
-      status: {
-          type: String,
-          enum: ["true", "false"]
-      }
-  }]
+    image: {
+        type: String,
+        default: ""
+    },
+    status: {
+        type: String,
+        enum: ["true", "false"]
+    }
 });
 
 schema.plugin(deepPopulate, {});
@@ -41,110 +27,110 @@ module.exports = mongoose.model('Gallery', schema);
 
 var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
 var model = {
-  getImages: function (data, callback) {
-      console.log(data);
-      Gallery.findOne({
-          _id: data._id
-      }).exec(function (err, found) {
-          if (err) {
-              // console.log(err);
-              callback(err, null);
-          } else {
-              // console.log(found,"000");
-              var data = {};
-              data.results = found.images;
-              if (found ) {
-                  callback(null, data);
-              } else {
-                  callback(null, {
-                      message: "No Data Found"
-                  });
-              }
-          }
+    getImages: function (data, callback) {
+        console.log(data);
+        Gallery.findOne({
+            _id: data._id
+        }).exec(function (err, found) {
+            if (err) {
+                // console.log(err);
+                callback(err, null);
+            } else {
+                // console.log(found,"000");
+                var data = {};
+                data.results = found.images;
+                if (found) {
+                    callback(null, data);
+                } else {
+                    callback(null, {
+                        message: "No Data Found"
+                    });
+                }
+            }
 
-      })
-  },
-  saveImages: function (data, callback) {
-      //  var product = data.product;
-      //  console.log(product);
-      // console.log("dddddd",data);
-      if (!data._id) {
-          Gallery.update({
-              _id: data.Gallery
-          }, {
-              $push: {
-                  images: data
-              }
-          }, function (err, updated) {
-              if (err) {
-                  console.log(err);
-                  callback(err, null);
-              } else {
-                  callback(null, updated);
-              }
-          });
-      } else {
-          data._id = objectid(data._id);
-          tobechanged = {};
-          var attribute = "images.$.";
-          _.forIn(data, function (value, key) {
-              tobechanged[attribute + key] = value;
-          });
-          Gallery.update({
-              "images._id": data._id
-          }, {
-              $set: tobechanged
-          }, function (err, updated) {
-              if (err) {
-                  console.log(err);
-                  callback(err, null);
-              } else {
-                  callback(null, updated);
-              }
-          });
-      }
-  },
-  getOneImages: function (data, callback) {
-      Gallery.aggregate([{
-          $unwind: "$images"
-      }, {
-          $match: {
-              "images._id": objectid(data._id)
-          }
-      }, {
-          $project: {
-              "images.image": 1,
-              "images.order": 1,
-              "images.status": 1,
-              "images._id": 1
-          }
-      }]).exec(function (err, found) {
-          if (err) {
-              console.log(err);
-              callback(err, null);
-          } else {
-              callback(null, found[0].images);
-          }
-      });
-  },
-  deleteImages: function (data, callback) {
-      Gallery.update({
-          "images._id": data._id
-      }, {
-          $pull: {
-              "images": {
-                  "_id": objectid(data._id)
-              }
-          }
-      }, function (err, updated) {
-          console.log(updated);
-          if (err) {
-              console.log(err);
-              callback(err, null);
-          } else {
-              callback(null, updated);
-          }
-      });
-  }
+        })
+    },
+    saveImages: function (data, callback) {
+        //  var product = data.product;
+        //  console.log(product);
+        // console.log("dddddd",data);
+        if (!data._id) {
+            Gallery.update({
+                _id: data.Gallery
+            }, {
+                $push: {
+                    images: data
+                }
+            }, function (err, updated) {
+                if (err) {
+                    console.log(err);
+                    callback(err, null);
+                } else {
+                    callback(null, updated);
+                }
+            });
+        } else {
+            data._id = objectid(data._id);
+            tobechanged = {};
+            var attribute = "images.$.";
+            _.forIn(data, function (value, key) {
+                tobechanged[attribute + key] = value;
+            });
+            Gallery.update({
+                "images._id": data._id
+            }, {
+                $set: tobechanged
+            }, function (err, updated) {
+                if (err) {
+                    console.log(err);
+                    callback(err, null);
+                } else {
+                    callback(null, updated);
+                }
+            });
+        }
+    },
+    getOneImages: function (data, callback) {
+        Gallery.aggregate([{
+            $unwind: "$images"
+        }, {
+            $match: {
+                "images._id": objectid(data._id)
+            }
+        }, {
+            $project: {
+                "images.image": 1,
+                "images.order": 1,
+                "images.status": 1,
+                "images._id": 1
+            }
+        }]).exec(function (err, found) {
+            if (err) {
+                console.log(err);
+                callback(err, null);
+            } else {
+                callback(null, found[0].images);
+            }
+        });
+    },
+    deleteImages: function (data, callback) {
+        Gallery.update({
+            "images._id": data._id
+        }, {
+            $pull: {
+                "images": {
+                    "_id": objectid(data._id)
+                }
+            }
+        }, function (err, updated) {
+            console.log(updated);
+            if (err) {
+                console.log(err);
+                callback(err, null);
+            } else {
+                callback(null, updated);
+            }
+        });
+    }
 };
 module.exports = _.assign(module.exports, exports, model);
